97aa971396858fc18cda6bb9550549a20dd6788d
nkaptx <nkaptx@163.com>
2023-01-19 17:12:01 +0800

    add AussPlugins impementation

---
+++ Source/AussPlugins/Private/AussPlugins.cpp
@@ -0,0 +1,24 @@
+#include "AussPlugins.h"
+#include "Log.h"
+
+#define LOCTEXT_NAMESPACE "FAussPluginsModule"
+
+void FAussPluginsModule::StartupModule()
+{
+#if WITH_AUSS
+	UE_LOG(LogAussPlugins, Log, TEXT("FAussPluginsModule startup"));
+#else
+#endif
+}
+
+void FAussPluginsModule::ShutdownModule()
+{
+#if WITH_AUSS
+	UE_LOG(LogAussPlugins, Log, TEXT("FAussPluginsModule shutdown"));
+#else
+#endif		
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FAussPluginsModule, AussPlugins)
