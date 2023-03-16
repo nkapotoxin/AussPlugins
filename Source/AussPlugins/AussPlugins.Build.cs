950f48f7158df61aee50e630c61c83a714127498
nkapotoxin <nkaptx@163.com>
2023-03-16 16:46:12 +0800

    fix ue shipping bugs

--- Source/AussPlugins/AussPlugins.Build.cs
+++ Source/AussPlugins/AussPlugins.Build.cs
@@ -37,22 +37,13 @@ public class AussPlugins : ModuleRules
 				"AUSS_RUNTIME_EXPORTS"
 			});
 
-		PublicIncludePaths.AddRange(
-			new[]
-			{
-				// ... add public include paths required here ...
-				Path.Combine(ModuleDirectory, "AussSdk/include"),
-				Path.Combine(ModuleDirectory, "AussSdk/source")
-			});
-
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "JsonUtilities", "Json"});
 		PrivateDependencyModuleNames.AddRange(new string[]{ "CoreUObject", "Engine", "Slate", "SlateCore"});
 
 		PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "includes"));
 
-		// TODO: Update cpp_redis tacopie to AussSDK later
-		PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "libs", "cpp_redis.lib"));
-		PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "libs", "tacopie.lib"));
+		// TODO: Update AussSdkLocal to AussSDK later
+		PublicAdditionalLibraries.Add(Path.Combine(ThirdPartyPath, "libs", "AussSdkLocal.lib"));
 
 		if (Target.Type == TargetRules.TargetType.Server)
 		{
